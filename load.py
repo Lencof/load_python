83bdec76b9257e9519782f829181c663eb5e657d
Lencof <71154457+Lencof@users.noreply.github.com>
2021-11-29 01:17:27 +0600

    Create load.py

---
+++ load.py
@@ -0,0 +1,13 @@
+import requests
+
+def save_file_from_www(link):
+    filename = link.split('/')[-1]
+    r = requests.get(link, allow_redirects=True)
+    open(filename, 'wb').write(r.content)
+
+link = 'https://raw.githubusercontent.com/Lencof/Shutdown-Python/main/Shutdown.py' # file directory
+
+save_file_from_www(link)
+
+import runpy
+runpy.run_module(mod_name="Shutdown.py") 
